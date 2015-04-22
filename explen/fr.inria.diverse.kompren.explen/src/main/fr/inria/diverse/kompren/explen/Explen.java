e4d264e4b08592bbbd15797accf93f1528fddeaa
arnobl <blouin.arno@gmail.com>
2015-04-22 17:17:02 +0200

    versions updated

--- explen/fr.inria.diverse.kompren.explen/src/main/fr/inria/diverse/kompren/explen/Explen.java
+++ explen/fr.inria.diverse.kompren.explen/src/main/fr/inria/diverse/kompren/explen/Explen.java
@@ -4,6 +4,7 @@ import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
 
 import fr.inria.diverse.kompren.explen.ui.ExplenFrame;
+import fr.inria.diverse.kompren.explen.view.ModelViewMapper;
 
 public class Explen {
 	public static void main(final String[] args) {
@@ -17,7 +18,7 @@ public class Explen {
 		frame.setVisible(true);
 		ToolTipManager.sharedInstance().setInitialDelay(10);
 		ToolTipManager.sharedInstance().setDismissDelay(100000);
-//		ModelViewMapper.getMapper().build("/media/data/dev/kompren/explen/fr.inria.diverse.kompren.explen/RAM.ecore");
+//		ModelViewMapper.getMapper().build("kdm-1.0.ecore");
 		frame.getCanvas().requestFocusInWindow();
 //		frame.getCanvas().setOperationsVisible(false);
 //		frame.getCanvas().update();
